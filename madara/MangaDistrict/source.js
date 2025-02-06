410c429048f4de79dfb170d076884acbff0d16b8
Matthew Ryan <96961818+mryan5072@users.noreply.github.com>
2025-02-05 21:58:16 -0800

    revert directoryPath test

--- madara/MangaDistrict/source.js
+++ madara/MangaDistrict/source.js
@@ -15776,7 +15776,7 @@ var _Sources = (() => {
       /**
        * When not using postIds, you need to set the directory path
        */
-      this.directoryPath = "read-scan";
+      this.directoryPath = "manga";
       /**
        * Some sources may redirect to the manga page instead of the chapter page if adding the parameter '?style=list'
        */
